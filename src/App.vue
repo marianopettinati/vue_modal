8fdcc022652f990d79d0b2f52053284ff42eb3b3
marianopettinati <marianopettinati@gmail.com>
2023-05-05 15:56:58 -0300

    pequeño proyecto de modal en vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <div v-if="showModal">
+    <Modal :theme="success" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Soy un link</a>
+        <a href="#">Soy otro link</a>
+      </template>
+      <h1>Success</h1>
+      <p>Soy un modal con la clase success</p>
+    </Modal>
+  </div>
+  <div v-if="showModalTwo">
+    <Modal :theme="warning" @close="toggleModalTwo">
+      <template v-slot>
+        <h1>WARNING</h1>
+        <p>Soy un modal con la clase warning</p>
+      </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Open modal</button>
+  <button @click="toggleModalTwo">Open Modal II</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "HandyMarian",
+      theme: "",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +56,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #2c3e50;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
